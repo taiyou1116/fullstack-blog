3650e031fcfc436f8593b33e477aa433161236cf
taiyou <hiromacha1116@icloud.com>
2023-10-27 16:23:40 +0900

    GETでposts取得

--- src/app/api/blog/route.ts
+++ src/app/api/blog/route.ts
@@ -1,6 +1,25 @@
+import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function main() {
+    try {
+        await prisma.$connect();
+    } catch(err) {
+        Error("DB接続に失敗しました");
+    }
+}
 
 // ブログを全て取得
 export const GET = async (req: Request, res: NextResponse) => {
-    console.log('Hello World');
+    try {
+        await main();
+        const posts = prisma.post.findMany();
+        return NextResponse.json({message: "success", posts}, {status: 200});
+    } catch(err) {
+        return NextResponse.json({message: "Err", err}, {status: 500});
+    } finally {
+        await prisma.$disconnect();
+    }
 }
