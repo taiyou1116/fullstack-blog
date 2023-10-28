2344536b25847fc43523c2a9422417f7a0db979e
taiyou <hiromacha1116@icloud.com>
2023-10-28 13:49:58 +0900

    記事取得

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,18 @@
+import { PostType } from "@/types";
 import Link from "next/link";
 
-export default function Home() {
+async function fetchAllBlogs() {
+  const res = await fetch(`http://localhost:3000/api/blog`, {
+    cache: "no-store", //SSR
+  })
+  
+  const data = await res.json();
+  return data.posts;
+}
+
+export default async function Home() {
+  const posts = await fetchAllBlogs();
+
   return (
     <main className="w-full h-full">
       <div className="md:w-2/4 sm:w-3/4 m-auto p-4 my-5 rounded-lg bg-blue-900 drop-shadow-xl">
@@ -18,28 +30,31 @@ export default function Home() {
         </Link>
       </div>
 
+      {/* 一記事 */}
       <div className="w-full flex flex-col justify-center items-center">
-        <div className="w-3/4 p-4 rounded-md mx-3 my-2 bg-slate-300 flex flex-col justify-center">
-          <div className="flex items-center my-3">
-            <div className="mr-auto">
-              <h2 className="mr-auto font-semibold">First Post</h2>
+        { posts.map((post: PostType) => (
+          <div key={post.id} className="w-3/4 p-4 rounded-md mx-3 my-2 bg-slate-300 flex flex-col justify-center">
+            <div className="flex items-center my-3">
+              <div className="mr-auto">
+                <h2 className="mr-auto font-semibold">{ post.title }</h2>
+              </div>
+              <Link
+                href={`/blog/edit/${ post.id }`}
+                className="px-4 py-1 text-center text-xl bg-slate-900 rounded-md font-semibold text-slate-200"
+              >
+                編集
+              </Link>
             </div>
-            <Link
-              href={`/blog/edit/1`}
-              className="px-4 py-1 text-center text-xl bg-slate-900 rounded-md font-semibold text-slate-200"
-            >
-              編集
-            </Link>
-          </div>
 
-          <div className="mr-auto my-1">
-            <blockquote className="font-bold text-slate-700">2023/8/17</blockquote>
-          </div>
+            <div className="mr-auto my-1">
+              <blockquote className="font-bold text-slate-700">{ new Date(post.date).toDateString() }</blockquote>
+            </div>
 
-          <div className="mr-auto my-1">
-            <h2>First Post Description</h2>
+            <div className="mr-auto my-1">
+              <h2>{ post.description }</h2>
+            </div>
           </div>
-        </div>
+        )) }
       </div>
     </main>
   )
---
+++ src/types.ts
@@ -0,0 +1,6 @@
+export type PostType = {
+    id: number,
+    title: String,
+    description: String,
+    date: Date,
+}
