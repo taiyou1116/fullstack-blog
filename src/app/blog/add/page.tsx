51c307695692431103c8d02ae076588f19459117
taiyou <hiromacha1116@icloud.com>
2023-10-28 17:57:43 +0900

    postpage雛形

---
+++ src/app/blog/add/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useRef } from 'react'
+
+const PostBlog = () => {
+  const titleRef = useRef<HTMLInputElement | null>(null);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    console.log(titleRef.current?.value);
+  }
+
+  return (
+    <div className="w-full m-auto flex my-4">
+      <div className="flex flex-col justify-center items-center m-auto">
+        <p className="text-2xl text-slate-500 font-bold p-3">ブログ新規作成 🚀</p>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <input
+            ref={titleRef}
+            placeholder="タイトルを入力"
+            type="text"
+            className="rounded-md px-4 w-full py-2 my-2"
+          />
+          <textarea
+            placeholder="記事詳細を入力"
+            className="rounded-md px-4 py-2 w-full my-2"
+          ></textarea>
+          <button className="font-semibold px-4 py-2 shadow-xl bg-slate-200 rounded-lg m-auto hover:bg-slate-100">
+            投稿
+          </button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default PostBlog;
